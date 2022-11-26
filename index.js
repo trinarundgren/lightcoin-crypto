9b338bd720fc194df2c87d04ad5ca2cbd3f0b88d
Trina <trina.rundgren@telus.com>
2022-11-25 19:56:50 -0800

    final version

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-let balance = 500.00;
+let balance = 0.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
-
-  commit() {
-    balance -= this.amount;
-  }
-
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance += this.value;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("mommysaurusrex");
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
